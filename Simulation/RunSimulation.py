237fda70a431ce74039775b57e877776f45832af
Carlo <CarloMehegan@gmail.com>
2024-09-21 18:30:38 -0400

    formatting and typehinting in RunSimulation

--- Simulation/RunSimulation.py
+++ Simulation/RunSimulation.py
@@ -2,7 +2,10 @@ import numpy as np
 import os
 from tqdm import tqdm
 
-def generate_simulation_results(num_iterations, output_filename, path=str):
+def generate_simulation_results(num_iterations: int,
+                                output_filename: str,
+                                path: str = "data"
+                               ) -> str:
     """
     Simulates shuffling a deck of red and black cards and saves the results as a .npy file.
 
@@ -12,28 +15,28 @@ def generate_simulation_results(num_iterations, output_filename, path=str):
     - path: str, optional, the directory where the file will be saved (default is 'fake_data')
     """
 
-    # Initialize deck
+    # initialize deck
     red = ['0'] * 26
     black = ['1'] * 26
     deck = black + red
     
-    np.random.seed(123)  # Optional: Set seed for reproducibility
+    np.random.seed(123)  # optionally, set seed for reproducibility
     
-    # Store results
+    # store results
     results = []
     for i in tqdm(range(num_iterations)):
         np.random.shuffle(deck)
-        results.append(int(''.join(deck), 2))  # Append binary as integer
+        results.append(int(''.join(deck), 2))  # append binary as integer
     
-    # Ensure the directory exists
+    # ensure the directory exists
     if not os.path.exists(path):
         os.makedirs(path)
     
-    # Construct the full file path
+    # construct the full file path
     full_filename = os.path.join(path, output_filename + '.npy')
     
-    # Save the results to a .npy file
+    # save the results to a .npy file
     np.save(full_filename, results)
     
-    print(f"Results saved to {full_filename}")
+    print(f"Shuffled decks saved to {full_filename}")
     return full_filename
